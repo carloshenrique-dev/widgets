3deab64ba2d7c96ef5d2f3303322c0f9741438ae
GabrielTorresPC <gabrielcabral@fitec.org.br>
2022-07-11 14:59:01 -0300

    first commit

---
+++ lib/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+
+      ],
+    );
+  }
+}
