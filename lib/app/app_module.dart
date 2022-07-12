a4403002a554ce527386f9590d659b30eb498378
carlostarabal <carlostarabal@fitec.org.br>
2022-07-12 08:13:18 -0300

    update

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:widgets/app/modules/initial/welcome_module.dart';
+import 'package:widgets/app/modules/welcome/welcome_module.dart';
 import 'modules/otp/otp_module.dart';
 import 'modules/splash/splash_page.dart';
 
