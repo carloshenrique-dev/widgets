e6425b8c56475436dd1ee40514f3e1cf5bf4b88a
Lucas Siqueira <lucassiqueira@fitec.org.br>
2022-07-13 12:07:20 -0300

    Fix to route

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:widgets/app/modules/clientless_purchase/clientless_purchase_module.dart';
 import 'package:widgets/app/modules/no_coverage_purchase/no_coverage_purchase_module.dart';
-import 'package:widgets/app/modules/no_coverage_purchase/no_coverage_purchase_page.dart';
+import 'package:widgets/app/modules/splash/splash_page.dart';
 
 import 'package:widgets/app/modules/welcome/welcome_module.dart';
 import 'modules/otp/otp_module.dart';
@@ -14,7 +14,7 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const NoCoveragePage()),
+        ChildRoute('/', child: (context, args) => const SplashPage()),
         ModuleRoute('/otp/', module: OtpModule()),
         ModuleRoute('/initial/', module: WelcomeModule()),
         ModuleRoute('/register/', module: RegisterModule()),
