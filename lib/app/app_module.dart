cb3277e711ba6494b66075d50415faf518ef6b05
Lucas Siqueira <lucassiqueira@fitec.org.br>
2022-07-14 14:46:56 -0300

    Fix route

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,9 +3,8 @@ import 'package:widgets/app/modules/boleto_generated/boleto_generated_module.dar
 import 'package:widgets/app/modules/clientless_purchase/clientless_purchase_module.dart';
 import 'package:widgets/app/modules/no_coverage_purchase/no_coverage_purchase_module.dart';
 import 'package:widgets/app/modules/personal_data/personal_data_module.dart';
-import 'package:widgets/app/modules/personal_data/personal_data_page.dart';
 import 'package:widgets/app/modules/readings/readings_module.dart';
-// import 'package:widgets/app/modules/splash/splash_page.dart';
+import 'package:widgets/app/modules/splash/splash_page.dart';
 
 import 'package:widgets/app/modules/welcome/welcome_module.dart';
 import 'modules/otp/otp_module.dart';
@@ -18,8 +17,8 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const PersonalDataPage()),
-        // ChildRoute('/', child: (context, args) => const SplashPage()),
+        //ChildRoute('/', child: (context, args) => const PersonalDataPage()),
+        ChildRoute('/', child: (context, args) => const SplashPage()),
         ModuleRoute('/otp/', module: OtpModule()),
         ModuleRoute('/initial/', module: WelcomeModule()),
         ModuleRoute('/register/', module: RegisterModule()),
